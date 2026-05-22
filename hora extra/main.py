ba8bc1e6dab32fc99ae34c34678ece22827d00da
Guilherme <guilhermeoliveira2938@gmail.com>
2026-05-22 20:40:39 -0300

    calculadora de hora extra

---
+++ hora extra/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template,  request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/calcular", methods=["POST"])
+def calcular():
+    sal_bruto = int(request.form["salario"])
+    porcent_dia = int(request.form["porcentagem"])
+    horas = int(request.form["horas"])
+
+    val_hora = sal_bruto / 220
+
+    if porcent_dia == 50:
+        val_extra = val_hora * 1.5
+    else:
+        val_extra = val_hora * 2
+
+    pagar = val_extra * horas
+
+    return render_template("index.html",
+        val_hora=round(val_hora, 2),
+        val_extra=round(val_extra, 2),
+        pagar=round(pagar, 2)
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
